d23bbe93ca5c3dfc4de0cf2b358ae23c879fa656
Yuhao <yban@usc.edu>
2022-08-02 21:05:14 -0700

    db

--- app/src/main/java/com/laioffer/tinnews/repository/NewsRepository.java
+++ app/src/main/java/com/laioffer/tinnews/repository/NewsRepository.java
@@ -1,8 +1,13 @@
 package com.laioffer.tinnews.repository;
 
+import android.os.AsyncTask;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
+import com.laioffer.tinnews.TinNewsApplication;
+import com.laioffer.tinnews.database.TinNewsDatabase;
+import com.laioffer.tinnews.model.Article;
 import com.laioffer.tinnews.model.NewsResponse;
 import com.laioffer.tinnews.network.NewsApi;
 import com.laioffer.tinnews.network.RetrofitClient;
@@ -14,9 +19,11 @@ import retrofit2.Response;
 public class NewsRepository {
 
     private final NewsApi newsApi;
+    private final TinNewsDatabase database;
 
     public NewsRepository() {
         this.newsApi = RetrofitClient.newInstance().create(NewsApi.class);
+        database = TinNewsApplication.getDatabase();
     }
 
     public LiveData<NewsResponse> getTopHeadlines(String country) {
@@ -60,5 +67,38 @@ public class NewsRepository {
                             }
                         });
         return everyThingLiveData;
+    }
+
+    public LiveData<Boolean> favoriteArticle(Article article) {
+        MutableLiveData<Boolean> resultLiveData = new MutableLiveData<>();
+        new FavoriteAsyncTask(database, resultLiveData).execute(article);
+        return resultLiveData;
+    }
+
+    private static class FavoriteAsyncTask extends AsyncTask<Article, Void, Boolean> {
+
+        private final TinNewsDatabase database;
+        private final MutableLiveData<Boolean> liveData;
+
+        private FavoriteAsyncTask(TinNewsDatabase database, MutableLiveData<Boolean> liveData) {
+            this.database = database;
+            this.liveData = liveData;
+        }
+
+        @Override
+        protected Boolean doInBackground(Article... articles) {
+            Article article = articles[0];
+            try {
+                database.articleDao().saveArticle(article);
+            } catch (Exception e) {
+                return false;
+            }
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean success) {
+            liveData.setValue(success);
+        }
     }
 }
